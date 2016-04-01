02a494d219908a1a7ccc2845f12e2a1471baf4d0
Brad L <blatendr@uvm.edu>
2016-04-01 13:41:51 -0400

    player class
    basic player class

---
+++ cs205_Player.java
@@ -0,0 +1,35 @@
+/*
+Brad LAtendresse, 
+cs 205
+Sorry game card logic
+*/
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.lang.Object;
+
+public class cs205_Player
+{
+   String name;
+   String color;
+   int comp_lvl;
+   double win_ratio;
+   
+   public cs205_Player(String n,String col,int comp)
+      {
+      name = n;
+      color=col;
+      comp_lvl=comp;
+      }
+   
+   public String getName()
+      {
+      return name;
+      }
+
+
+
+
+
+}
